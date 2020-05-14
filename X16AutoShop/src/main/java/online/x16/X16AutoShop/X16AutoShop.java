d18be91b71aa053491968b9b6da9c0cae6d07d83
Adam W <Code.AdamW78@gmail.com>
2020-05-14 17:34:37 -0400

    Added proper imports

--- X16AutoShop/src/main/java/online/x16/X16AutoShop/X16AutoShop.java
+++ X16AutoShop/src/main/java/online/x16/X16AutoShop/X16AutoShop.java
@@ -1,5 +1,9 @@
 package online.x16.X16AutoShop;
+import java.util.logging.Level;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import online.x16.X16AutoShop.tools.Colorizer;
 
 public class X16AutoShop extends JavaPlugin {
 
@@ -15,13 +19,11 @@ public class X16AutoShop extends JavaPlugin {
 	public void onDisable() {
 		
 	}
-	/*
 	public void log(Object obj){
         if(getConfig().getBoolean("color-logs", true)){
-            getServer().getConsoleSender().sendMessage(MessageUtil.colorize("&3[&d" + getName() + "&3] &r" + obj));
+            getServer().getConsoleSender().sendMessage(Colorizer.colorize("&3[&d" + getName() + "&3] &r" + obj));
         }else{
-            Bukkit.getLogger().log(Level.INFO, "[" + getName() + "] " + MessageUtil.colorize((String) obj).replaceAll("(?)\u00a7([a-f0-9k-or])", ""));
+            Bukkit.getLogger().log(Level.INFO, "[" + getName() + "] " + Colorizer.colorize((String) obj).replaceAll("(?)\u00a7([a-f0-9k-or])", ""));
         }
     }
-    */
 }
