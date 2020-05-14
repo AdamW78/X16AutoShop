ede19025d8626cc572373ba8c9bf402dd446c634
Adam W <Code.AdamW78@gmail.com>
2020-05-13 23:22:28 -0400

    Added X16AutoShop Object & colored console output code (commented)

--- X16AutoShop/src/main/java/X16AutoShop.java
+++ X16AutoShop/src/main/java/X16AutoShop.java
@@ -2,6 +2,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class X16AutoShop extends JavaPlugin {
 
+	public X16AutoShop() { 
+		
+	}
 	@Override
 	public void onEnable() {
 		
@@ -11,4 +14,13 @@ public class X16AutoShop extends JavaPlugin {
 	public void onDisable() {
 		
 	}
+	/*
+	public void log(Object obj){
+        if(getConfig().getBoolean("color-logs", true)){
+            getServer().getConsoleSender().sendMessage(MessageUtil.colorize("&3[&d" + getName() + "&3] &r" + obj));
+        }else{
+            Bukkit.getLogger().log(Level.INFO, "[" + getName() + "] " + MessageUtil.colorize((String) obj).replaceAll("(?)\u00a7([a-f0-9k-or])", ""));
+        }
+    }
+    */
 }
